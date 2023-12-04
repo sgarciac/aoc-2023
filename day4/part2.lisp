a416d2aab18c8fa40a9bdec41b26bc628eed9e40
Sergio <sergio@demarque.com>
2023-12-04 06:30:26 -0500

    perf

--- day4/part2.lisp
+++ day4/part2.lisp
@@ -25,13 +25,12 @@
           finally (return (make-array (length parsed-lines) :initial-contents parsed-lines))
           )))
 
-(loop
-  with input = (read-input "input")
-  for i from 0 upto (1- (length input))
-do (let ((card (aref input i)))
-     (dotimes (j (card-copies card))
-       (loop
-         for k from (+ i 1) upto (min (1- (length input)) (+ i (card-mp card)))
-         do (when (< k (length input))
-              (incf (card-copies (aref input k)))))))
-  finally (return (loop for card across input summing (card-copies card))))
+(time (loop
+        with input = (read-input "input")
+        for i from 0 upto (1- (length input))
+        do (let ((card (aref input i)))
+             (loop
+               for k from (+ i 1) upto (min (1- (length input)) (+ i (card-mp card)))
+               do (when (< k (length input))
+                    (incf (card-copies (aref input k)) (card-copies card)))))
+        finally (return (loop for card across input summing (card-copies card)))))
